88e118dcacf0f379bf74ad9bd5a32ca6215a6f6a
Ale <alessandra.na001@gmail.com>
2021-10-04 18:17:06 -0500

    username pasa de login a main

---
+++ app/src/main/java/com/example/cookiemakerapp/LoginActivity.kt
@@ -0,0 +1,28 @@
+package com.example.cookiemakerapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val txtareaUsername: EditText = findViewById(R.id.txtareaUsername)
+
+        val btnLogin : Button = findViewById(R.id.btnLogin)
+        btnLogin.setOnClickListener{
+            val intent : Intent = Intent()
+            intent.setClass(this, MainActivity::class.java)
+            // pasándole el username
+            val bundle : Bundle = Bundle()
+            bundle.putString("username", txtareaUsername.text.toString())
+            intent.putExtra("datalogin",bundle)
+            startActivity(intent)
+
+        }
+    }
+}
--- app/src/main/java/com/example/cookiemakerapp/MainActivity.kt
+++ app/src/main/java/com/example/cookiemakerapp/MainActivity.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    var username: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
+        username = intent.getBundleExtra("datalogin")?.getString("username")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println(username)
     }
 }
